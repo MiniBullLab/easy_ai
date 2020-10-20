23c8820fdc5819a281e124fbb9cf513bdfd8884d
wfw112233 <1336946993@qq.com>
2020-10-20 19:20:27 +0800

    fix easy_converter scripts

--- scripts/easy_converter/keypoint_caffe_infer.sh
+++ scripts/easy_converter/keypoint_caffe_infer.sh
@@ -1,1 +1,2 @@
+# keypoint caffe inference
 python3 easy_converter/inference/det2d/caffe_keypoints2d.py -i /home/lpj/Desktop/3D-Pose/backup/object/test/pr -p /home/lpj/Desktop/3D-Pose/yolo-pose_san.prototxt -m /home/lpj/Desktop/3D-Pose/yolo-pose_san.caffemodel -o layer30-conv
