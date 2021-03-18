0e8a86fd7db16dfff96436cadf166b801ba87475
lipeijie <lipeijie1@geely.com>
2021-03-18 19:37:02 +0800

    lipeijie update pose2d

---
+++ easyai/config/task/det_pose2d_config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:lipeijie
+
+import os
+from easyai.base_name.task_name import TaskName
+from easyai.config.utility.base_config import BaseConfig
+from easyai.config.utility.registry import REGISTERED_TASK_CONFIG
