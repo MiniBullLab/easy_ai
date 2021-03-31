a0871ba52b79be2b151688da7296981a1fbd09e7
lipeijie <lipeijie1@geely.com>
2021-03-31 16:57:12 +0800

    lipeijie update one class

---
+++ easyai/tasks/one_class/__init__.py
@@ -0,0 +1,3 @@
+from . import one_class
+from . import one_class_test
+from . import one_class_train
--- easyai/tools/sample_tool/create_one_class_sample.py
+++ easyai/tools/sample_tool/create_one_class_sample.py
@@ -6,8 +6,6 @@ import os
 import sys
 sys.path.insert(0, os.getcwd() + "/..")
 
-import codecs
-import json
 import random
 import numpy as np
 import cv2
@@ -15,7 +13,7 @@ from easyai.helper.dirProcess import DirProcess
 from easyai.helper.arguments_parse import ToolArgumentsParse
 
 
-class CreateClassifySample():
+class CreateOneClassSample():
 
     def __init__(self):
         self.dir_process = DirProcess()
@@ -28,10 +26,13 @@ class CreateClassifySample():
         elif "train" == flag.strip():
             self.process_train(input_dir, output_dir, flag)
         elif "val" == flag.strip():
-            self.process_train(input_dir, output_dir, flag)
+            self.process_val(input_dir, output_dir, flag)
 
     def process_train_val(self, input_dir, output_dir, probability):
-        data_class = self.get_data_class(input_dir)
+        intput_path, ok_dir_name = os.path.split(input_dir)
+        data_class = self.get_data_class(intput_path)
+
+        assert len(data_class) == 2
 
         save_train_path = os.path.join(output_dir, "train.txt")
         save_val_path = os.path.join(output_dir, "val.txt")
@@ -42,40 +43,61 @@ class CreateClassifySample():
         save_val_file = open(save_val_path, "w")
 
         for class_index, class_name in enumerate(data_class):
-            data_class_dir = os.path.join(input_dir, class_name)
-            image_list = list(self.dir_process.getDirFiles(data_class_dir,
-                                                           "*.*"))
+            data_class_dir = os.path.join(intput_path, class_name)
+            image_list = list(self.dir_process.getDirFiles(data_class_dir, "*.*"))
             random.shuffle(image_list)
-            for image_index, image_path in enumerate(image_list):
-                # print(image_path)
-                if (image_index + 1) % probability == 0:
-                    self.write_data(image_path, class_name, class_index, save_val_file)
-                else:
-                    self.write_data(image_path, class_name, class_index, save_train_file)
+            if class_name == ok_dir_name:
+                for image_index, image_path in enumerate(image_list):
+                    # print(image_path)
+                    if (image_index + 1) % probability == 0:
+                        self.write_data(image_path, class_name, 0, save_val_file)
+                    else:
+                        self.write_data(image_path, class_name, 0, save_train_file)
+            else:
+                for image_index, image_path in enumerate(image_list):
+                    # print(image_path)
+                    self.write_data(image_path, class_name, 0, save_val_file)
 
         save_train_file.close()
         save_val_file.close()
-        self.write_data_class(data_class, output_dir)
 
     def process_train(self, input_dir, output_dir, flag):
-        data_class = self.get_data_class(input_dir)
-
+        intput_path, ok_dir_name = os.path.split(input_dir)
         save_train_path = os.path.join(output_dir, "%s.txt" % flag)
         if os.path.exists(save_train_path):
             return
         save_train_file = open(save_train_path, "w")
 
-        for class_index, class_name in enumerate(data_class):
-            data_class_dir = os.path.join(input_dir, class_name)
-            image_list = list(self.dir_process.getDirFiles(data_class_dir,
-                                                           "*.*"))
-            random.shuffle(image_list)
-            for image_index, image_path in enumerate(image_list):
-                # print(image_path)
-                self.write_data(image_path, class_name, class_index, save_train_file)
-
+        image_list = list(self.dir_process.getDirFiles(input_dir, "*.*"))
+        random.shuffle(image_list)
+        for image_index, image_path in enumerate(image_list):
+            # print(image_path)
+            self.write_data(image_path, ok_dir_name, 0, save_train_file)
         save_train_file.close()
-        self.write_data_class(data_class, output_dir)
+
+    def process_val(self, input_dir, output_dir, flag):
+        intput_path, ok_dir_name = os.path.split(input_dir)
+        data_class = self.get_data_class(intput_path)
+        assert len(data_class) == 2
+        save_val_path = os.path.join(output_dir, "%s.txt" % flag)
+        if os.path.exists(save_val_path):
+            return
+        save_val_file = open(save_val_path, "w")
+
+        for class_index, class_name in enumerate(data_class):
+            data_class_dir = os.path.join(intput_path, class_name)
+            image_list = list(self.dir_process.getDirFiles(data_class_dir, "*.*"))
+            random.shuffle(image_list)
+            if class_name == ok_dir_name:
+                for image_index, image_path in enumerate(image_list):
+                    # print(image_path)
+                    self.write_data(image_path, class_name, 0, save_val_file)
+            else:
+                for image_index, image_path in enumerate(image_list):
+                    # print(image_path)
+                    self.write_data(image_path, class_name, 0, save_val_file)
+
+        save_val_file.close()
 
     def write_data(self, image_path, class_name, class_index, save_file):
         path, fileNameAndPost = os.path.split(image_path)
@@ -96,21 +118,11 @@ class CreateClassifySample():
                     result.append(name)
         return sorted(result)
 
-    def write_data_class(self, data_class, output_dir):
-        class_define = {}
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-        for index, className in enumerate(data_class):
-            class_define[index] = className
-        save_class_path = os.path.join(output_dir, "class.json")
-        with codecs.open(save_class_path, 'w', encoding='utf-8') as f:
-            json.dump(class_define, f, sort_keys=True, indent=4, ensure_ascii=False)
-
 
 def main():
     print("start...")
     options = ToolArgumentsParse.process_sample_parse()
-    test = CreateClassifySample()
+    test = CreateOneClassSample()
     test.process_sample(options.inputPath,
                         options.outputPath,
                         options.type,
@@ -120,4 +132,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
