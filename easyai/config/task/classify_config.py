f920fc3ac3d86d17656413552579fdbb00f61aee
lpj0822 <1014153254@qq.com>
2021-06-17 15:17:50 +0800

    lipeijie update bug

--- easyai/config/task/classify_config.py
+++ easyai/config/task/classify_config.py
@@ -37,7 +37,6 @@ class ClassifyConfig(CommonTrainConfig):
 
     def save_train_value(self, config_dict):
         self.save_image_train_value(config_dict)
-        config_dict['train_data_augment'] = self.train_data_augment
 
     def get_data_default_value(self):
         self.data = {'image_size': (224, 224),  # W * H
