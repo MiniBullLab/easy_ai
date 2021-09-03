c7d552791f9d4afc0898ca48c95e8b611cf5515c
lpj0822 <1014153254@qq.com>
2021-09-03 18:30:42 +0800

    lipeijie update one class

--- easy_tools/test_gui.py
+++ easy_tools/test_gui.py
@@ -2,6 +2,10 @@
 # -*- coding:utf-8 -*-
 # Author:lipeijie
 
+from easyai.utility.logger import EasyLogger
+log_file_path = EasyLogger.get_log_file_path("test_gui.log")
+EasyLogger.init(logfile_level="debug", log_file=log_file_path, stdout_level="error")
+
 import sys
 from PyQt5.QtWidgets import QApplication
 from easy_tools.model_test.pc_arm_window import PCAndArmTestWindow
